6e4a36bc626c5e6e453fb9a364ff70e84a5e29d8
Patrick Bucher <patrick.bucher@mailbox.org>
2024-02-18 09:50:23 +0100

    Gale-Shapley algorithm with stability check

---
+++ matching.py
@@ -0,0 +1,45 @@
+from stability import blocking_pairs
+
+prefs_m = {
+    1: [4, 1, 2, 3],
+    2: [2, 3, 1, 4],
+    3: [2, 4, 3, 1],
+    4: [3, 1, 4, 2],
+}
+
+prefs_w = {
+    1: [4, 1, 3, 2],
+    2: [1, 3, 2, 4],
+    3: [1, 2, 3, 4],
+    4: [4, 1, 3, 2],
+}
+
+
+# Gale-Shapley matching algorithm
+def match(pm, pw):
+    assert len(pm) == len(pw)
+    n = len(pm)
+
+    pairs = []
+    while len(pairs) < n:
+        for m, prefs in prefs_m.items():
+            for w in prefs:
+                if not any(filter(lambda p: p[1] == w, pairs)):
+                    pairs.append((m, w))
+                    break
+                else:
+                    m_old = next(filter(lambda p: p[1] == w, pairs))[0]
+                    p_old = prefs_w[w].index(m_old)
+                    p_new = prefs_w[w].index(m)
+                    if p_new < p_old:
+                        pairs.remove((m_old, w))
+                        pairs.append((m, w))
+                        break
+    return sorted(pairs)
+
+
+if __name__ == '__main__':
+    pairs = match(prefs_m, prefs_w)
+    print('matching', pairs)
+    blocking = blocking_pairs(pairs, prefs_m, prefs_w)
+    print('blocking', blocking)
--- stability.py
+++ stability.py
@@ -1,36 +1,19 @@
-# Example from p. 6/7: Six blocking pairs are found.
-# >>> blocking_pairs(matching)
-# [(1, 2), (1, 4), (2, 1), (2, 4), (3, 2), (4, 4)]
-
-prefs_m = {
-    1: [2, 4, 1, 3],
-    2: [3, 1, 4, 2],
-    3: [2, 3, 1, 4],
-    4: [4, 1, 3, 2],
-}
-prefs_w = {
-    1: [2, 1, 4, 3],
-    2: [4, 3, 1, 2],
-    3: [1, 4, 3, 2],
-    4: [2, 1, 4, 3],
-}
-
-matching = set([(1, 1), (2, 2), (3, 4), (4, 3)])
-
-
 # Determines if the unmatched pair (l, r) is unstable considering the matching
 # and the individual preferences prefs_m and prefs_w for l and r, respectively.
-def is_blocking(l, r):
-    actual_r = list(filter(lambda p: p[0] == l, matching))[0][1]
-    actual_l = list(filter(lambda p: p[1] == r, matching))[0][0]
+def is_blocking(l, r, matching, prefs_m, prefs_w):
+    actual_r = next(filter(lambda p: p[0] == l, matching))[1]
+    actual_l = next(filter(lambda p: p[1] == r, matching))[0]
     l_prefers_other = prefs_m[l].index(r) < prefs_m[l].index(actual_r)
     r_prefers_other = prefs_w[r].index(l) < prefs_w[r].index(actual_l)
     return l_prefers_other and r_prefers_other
 
 
 # Returns the blocking (unrealized) pairs in the (realized) matching.
-def blocking_pairs(matching):
-    return list(filter(lambda p: is_blocking(p[0], p[1]), unrealized_pairs(matching)))
+def blocking_pairs(matching, prefs_m, prefs_w):
+    return list(
+        filter(
+            lambda p: is_blocking(p[0], p[1], matching, prefs_m, prefs_w),
+            unrealized_pairs(matching)))
 
 
 def unrealized_pairs(matching):
@@ -40,3 +23,26 @@ def unrealized_pairs(matching):
         for j in range(1, n+1):
             all_pairs.append((i, j))
     return sorted(list(filter(lambda p: p not in matching, all_pairs)))
+
+
+if __name__ == '__main__':
+    # Example from p. 6/7: Six blocking pairs are found.
+    # >>> blocking_pairs(matching)
+    # [(1, 2), (1, 4), (2, 1), (2, 4), (3, 2), (4, 4)]
+
+    prefs_m = {
+        1: [2, 4, 1, 3],
+        2: [3, 1, 4, 2],
+        3: [2, 3, 1, 4],
+        4: [4, 1, 3, 2],
+    }
+    prefs_w = {
+        1: [2, 1, 4, 3],
+        2: [4, 3, 1, 2],
+        3: [1, 4, 3, 2],
+        4: [2, 1, 4, 3],
+    }
+
+    matching = set([(1, 1), (2, 2), (3, 4), (4, 3)])
+    print('matching', matching)
+    print('blocking', blocking_pairs(matching, prefs_m, prefs_w))
